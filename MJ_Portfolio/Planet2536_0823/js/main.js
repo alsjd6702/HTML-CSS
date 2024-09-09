3cbc4dd6bc059a5ed248d5f3250c05aeccd3caef
min <alsjd6702@gmail.com>
2024-09-09 17:39:35 +0900

    first commit

--- MJ_Portfolio/Planet2536_0823/js/main.js
+++ MJ_Portfolio/Planet2536_0823/js/main.js
@@ -4,7 +4,7 @@ function logoopacity(){
 window.onscroll = function(){
     let ht = document.documentElement.scrollTop;
     let logo = document.querySelector(".intro .logo img");
-    console.log(ht);
+    // console.log(ht);
     if(ht > 350){
         logo.classList.add("on");
     } if(ht > 550){                
@@ -13,3 +13,7 @@ window.onscroll = function(){
         logo.classList.add("on3");
     }
 }
+let planned = document.querySelector("div.planned");
+let decided = document.querySelector("div.decided");
+let conInfo = document.querySelectorAll("div.per");
+let classImg = document.querySelectorAll("div.class_img");
